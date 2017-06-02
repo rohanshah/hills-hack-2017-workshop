5a4f49c0ba39ed337ecc463b2045b917dd0ba96a
Rohan Shah <rshah@rjmetrics.com>
2017-06-02 15:51:32 -0400

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+### Dependencies
+- Docker
+- [Optional] ngrock
+
+
+### Run
+
+```
+docker build -t chat-app .
+docker run -d -p 3000:3000 chat-app
+```
---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    var socket = io();
+
+    $('#b').click(function(){
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      return false;
+    });
+
+    socket.on('chat message', function(msg){
+      $('#messages').append($('<li>').text(msg));
+    });
+
+});
+

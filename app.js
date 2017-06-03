038b795dd62bba226ae86b6a67b7aaf37c499e90
Rohan Shah <rshah@rjmetrics.com>
2017-06-03 08:58:41 -0400

    delete the code

--- app.js
+++ app.js
@@ -4,23 +4,14 @@ $(document).ready(function() {
     // - create a Socket.IO client 
     // - have it emit something to the server
     // - have it listen for a message from the server
-    var socket = io();
 
     // Step (1a): https://api.jquery.com/click/
     // - use JQuery to select the button and do something when it's clicked
-    $('#b').click(function() {
       // Step (1b):
       // - use val to read something from the DOM and do something with it
-      socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-      return false;
-    });
 
-    socket.on('chat message', function(msg) {
       // Step (2): http://api.jquery.com/append/
       // - Update the dom using append
-      $('#messages').append($('<li>').text(msg));
-    });
 
 });
 

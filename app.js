f9420a7ffd034551d9d44ae18316f7b7b83e78ce
Rohan Shah <rshah@rjmetrics.com>
2017-06-03 09:01:19 -0400

    finish up tutorial comments

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 
+    // Step (1a): https://api.jquery.com/click/
+    // - use JQuery to select the button and do something when it's clicked
+    // Step (1b):
+    // - use val to read something from the DOM and do something with it
+
+    // Step (2): http://api.jquery.com/append/
+    // - Update the dom using append
+
     // Step (3): https://socket.io/docs/
     // - create a Socket.IO client 
     // - have it emit something to the server
     // - have it listen for a message from the server
-
-    // Step (1a): https://api.jquery.com/click/
-    // - use JQuery to select the button and do something when it's clicked
-      // Step (1b):
-      // - use val to read something from the DOM and do something with it
-
-      // Step (2): http://api.jquery.com/append/
-      // - Update the dom using append
 
 });
 
